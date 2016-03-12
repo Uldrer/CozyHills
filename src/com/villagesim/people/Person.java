6cdab307b24f3d66460f7725c47e3bc095e2e43c
Martin Lillieborg <uldrer@gmail.com>
2016-03-12 18:45:11 +0100

    Updating draw method for persons.

--- src/com/villagesim/people/Person.java
+++ src/com/villagesim/people/Person.java
@@ -1,12 +1,17 @@
 package com.villagesim.people;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.geom.Point2D;
+import java.util.Random;
 
+import com.villagesim.Const;
 import com.villagesim.interfaces.Drawable;
 
 public class Person implements PersonInterface, Drawable {
 
 	private int id; // unique person id
+	private Point2D coordinate; // location
 	private double nutrition; // low values are hunger and starvation, 0 is death
 	private double aqua; // low values are thirst and dehydration, 0 is death
 	private double nutrition_decline_rate;
@@ -17,6 +22,7 @@ public class Person implements PersonInterface, Drawable {
 	private final double NUTRITION_DECLINE_TIME_S = 18114400; // Assumption, death after 3 weeks without food
 	private final double AQUA_DECLINE_TIME_S = 259200; // Assumption, death after 3 days without water
 	private final double SECONDS_PER_DAY = 86400;
+	private final int PERSON_SIZE = 3;
 	
 	private static int id_counter = 0;
 	
@@ -27,6 +33,7 @@ public class Person implements PersonInterface, Drawable {
 		aqua = MAX_AQUA_POINTS;
 		nutrition_decline_rate = MAX_NUTRITION_POINTS/NUTRITION_DECLINE_TIME_S;
 		aqua_decline_rate = MAX_AQUA_POINTS/AQUA_DECLINE_TIME_S;
+		generateCoordinate();
 	}
 	
 	public boolean IsAlive()
@@ -43,9 +50,10 @@ public class Person implements PersonInterface, Drawable {
 	}
 	
 	@Override
-	public void draw(Graphics bbg) {
-		// TODO Auto-generated method stub
-		
+	public void draw(Graphics bbg) 
+	{
+		bbg.setColor(Color.BLACK);
+		bbg.fillOval((int)(coordinate.getX()+0.5), (int)(coordinate.getY()+0.5), PERSON_SIZE, PERSON_SIZE);
 	}
 
 	@Override
@@ -60,13 +68,23 @@ public class Person implements PersonInterface, Drawable {
 		nutrition -= nutrition_decline_rate*seconds;
 		aqua -= aqua_decline_rate*seconds;
 		lifetime_days += seconds/SECONDS_PER_DAY;
-		
 	}
 
 	@Override
 	public void UpdateSensorReadings() {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	private Point2D generateCoordinate()
+	{
+		Point2D coord = new Point2D.Double();
+		Random rand = new Random();
+		
+		int x = rand.nextInt(Const.WINDOW_WIDTH-PERSON_SIZE);
+		int y = rand.nextInt(Const.WINDOW_HEIGHT-PERSON_SIZE);
+		coord.setLocation(x, y);
+		return coord;
 	}
 
 }
