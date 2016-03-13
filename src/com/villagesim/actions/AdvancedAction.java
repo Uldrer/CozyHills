718c535e5668ce51e488ccbca368f3a32ba52b7f
Martin Lillieborg <uldrer@gmail.com>
2016-03-13 12:35:35 +0100

    Added rank to advanced actions for now, later we can implement some other descisionmaking process.

--- src/com/villagesim/actions/AdvancedAction.java
+++ src/com/villagesim/actions/AdvancedAction.java
@@ -3,30 +3,33 @@ package com.villagesim.actions;
 public enum AdvancedAction implements ActionInterface{
 	
 	// Gathering, add more when needed
-	WATER("Gather water", BasicAction.GATHER),
-	FISH("Fish", BasicAction.GATHER),
-	BERRIES("Gather berries", BasicAction.GATHER),
-	HUNT("Hunt", BasicAction.GATHER),
-	NUTS("Gather nuts", BasicAction.GATHER),
-	WOOD("Gather wood", BasicAction.GATHER),
+	WATER("Gather water", BasicAction.GATHER, 1),
+	FISH("Fish", BasicAction.GATHER, 2),
+	NUTS("Gather nuts", BasicAction.GATHER, 3),
+	BERRIES("Gather berries", BasicAction.GATHER, 4),
+	HUNT("Hunt", BasicAction.GATHER, 5),
+	WOOD("Gather wood", BasicAction.GATHER, 6),
 	
 	// Movement, add more when needed
-	SWIM("Swim", BasicAction.MOVE),
-	RUN("Run", BasicAction.MOVE),
-	WALK("Walk", BasicAction.MOVE),
-	CLIMB("Climb", BasicAction.MOVE),
+	WALK("Walk", BasicAction.MOVE, 1),
+	RUN("Run", BasicAction.MOVE, 2),
+	SWIM("Swim", BasicAction.MOVE, 3),
+	CLIMB("Climb", BasicAction.MOVE, 4),
 	
 	// Work, add more when needed
-	TRANSPORT_GOODS("Transport goods", BasicAction.WORK),
-	BUILD("Build", BasicAction.WORK),
-	CUT_TREES("Cut trees", BasicAction.WORK);
+	TRANSPORT_GOODS("Transport goods", BasicAction.WORK, 1),
+	CUT_TREES("Cut trees", BasicAction.WORK, 2),
+	BUILD("Build", BasicAction.WORK, 3);
+	
 
 	private final String info;
     private final BasicAction basicAction;
+    private final int rank;
 	
-	private AdvancedAction(String info, BasicAction basicAction) {
+	private AdvancedAction(String info, BasicAction basicAction, int rank) {
         this.info = info;
         this.basicAction = basicAction;
+        this.rank = rank;
     }
 	
 	@Override
@@ -42,6 +45,11 @@ public enum AdvancedAction implements ActionInterface{
 	public String getInfo()
 	{
 		return info;
+	}
+	
+	public int getRank()
+	{
+		return rank;
 	}
 	
 }
