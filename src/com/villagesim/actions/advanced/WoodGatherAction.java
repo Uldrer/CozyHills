28b779ddea1f88d14105354a1769487d0c824c3f
Martin Lillieborg <uldrer@gmail.com>
2016-04-02 11:43:38 +0200

    Added stub for wood gathering.

---
+++ src/com/villagesim/actions/advanced/WoodGatherAction.java
@@ -0,0 +1,30 @@
+package com.villagesim.actions.advanced;
+
+import com.villagesim.interfaces.Action;
+import com.villagesim.people.Person;
+
+public class WoodGatherAction implements Action {
+
+	private Person person;
+	
+	public WoodGatherAction(Person person)
+	{
+		this.person = person;
+	}
+	
+	@Override
+	public void execute(int seconds) {
+		// TODO Auto-generated method stub
+		if(person.printDebug())
+		{
+			System.out.println("Person id: " + person.getId() + " is gathering wood.");
+		}
+	}
+
+	@Override
+	public boolean isValid() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
