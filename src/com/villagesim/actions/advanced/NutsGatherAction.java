d76e57603a07a147e57e83eff9ff2a50d0bc999a
Martin Lillieborg <uldrer@gmail.com>
2016-04-02 13:11:19 +0200

    Implementing nuts gathering action.

--- src/com/villagesim/actions/advanced/NutsGatherAction.java
+++ src/com/villagesim/actions/advanced/NutsGatherAction.java
@@ -1,20 +1,44 @@
 package com.villagesim.actions.advanced;
 
+import com.villagesim.Const;
+import com.villagesim.areas.Area;
+import com.villagesim.areas.Storage;
 import com.villagesim.interfaces.Action;
 import com.villagesim.people.Person;
+import com.villagesim.resources.Nuts;
+import com.villagesim.sensors.Sensor;
+import com.villagesim.sensors.SensorHelper;
 
 public class NutsGatherAction implements Action {
 
 	private Person person;
+	private Sensor distSensor;
+	private double gather_rate; // amount/s
 	
 	public NutsGatherAction(Person person)
 	{
 		this.person = person;
+		this.distSensor = Sensor.DIST_TO_WILD_FOOD;
+		this.gather_rate = 2/Const.SECONDS_PER_HOUR; // Assumption, you can gather 2 liters of nuts per hour on average
 	}
 	
 	@Override
 	public void execute(int seconds) {
-		// TODO Auto-generated method stub
+		// Consume resource according to gathering rate
+		double potentialAmount = gather_rate*seconds;
+		
+		// Check if that is possible
+		Area area = person.getClosestArea(distSensor.getIndex());
+		double availableValue = area.getResourceAmountValue(Nuts.class);
+		double value = potentialAmount <= availableValue ? potentialAmount : availableValue;
+		
+		// Remove that value from resource
+		area.consumeResourceAmountValue(Nuts.class, value);
+		
+		// Fill personal storage with equal amount
+		Storage personalStorage = person.getPersonalStorage();
+		personalStorage.fillResourceAmountValue(Nuts.class, value);
+		
 		if(person.printDebug())
 		{
 			System.out.println("Person id: " + person.getId() + " is gathering nuts.");
@@ -23,8 +47,11 @@ public class NutsGatherAction implements Action {
 
 	@Override
 	public boolean isValid() {
-		// TODO Auto-generated method stub
-		return false;
+		double distanceToResource = person.getSensorReading(distSensor.getIndex());
+		
+		boolean valid = SensorHelper.isNormalizedDistanceCloseEnoughForAction(distanceToResource);
+
+		return valid;
 	}
 
 }
